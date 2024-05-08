6f2d841d05fe724389d4ca0846eb799152dc99b4
gelasca <146396130+gelasca@users.noreply.github.com>
2024-05-08 13:23:27 +0000

    fourth commit

--- novo_projeto/heroes/src/app/paginas/lista/lista.component.ts
+++ novo_projeto/heroes/src/app/paginas/lista/lista.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.css'
 })
-export class ListaComponent {
 
+export class ListaComponent {
+  constructor(private http: HttpClient) {
+
+  }
+
+  listaHeroes: any = null
+  
+  getHeroes() {
+    this.http.get('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json')
+    .subscribe({
+      next: (data: any)=>{
+        console.log(data[0])
+      },
+      error: (error: any)=>{
+        console.log(error)
+      }
+    })
+  }
 }
